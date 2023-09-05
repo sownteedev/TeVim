3ac0f3158cbe75dd462fe350b221622416c6091b
sownteedev <sownteenguyen@gmail.com>
2023-09-05 15:51:53 +0700

    test

--- lua/themes/integrations/diags.lua
+++
@@ -1,9 +0,0 @@
-return function(colors)
-    return {
-        DiagnosticError = { fg = colors.red, italic = true, bold = true },
-        DiagnosticWarn = { fg = colors.yellow, italic = true, bold = true },
-        DiagnosticInfo = { fg = colors.blue, italic = true, bold = true },
-        DiagnosticHint = { fg = colors.purple, italic = true, bold = true },
-        DiagnosticInformation = { fg = colors.green, italic = true, bold = true },
-    }
-end
--- lua/themes/integrations/noice.lua
+++
@@ -1,9 +0,0 @@
-return function(colors)
-    return {
-        NoiceCmdlinePopup = { bg = colors.darker_black },
-        NoiceCmdlinePopupBorder = { bg = colors.darker_black, fg = colors.darker_black },
-        NoiceCmdlinePopupBorderSearch = { link = "NoiceCmdlinePopupBorder" },
-        NoiceCmdlinePopupTitle = { bg = colors.darker_black, fg = colors.darker_black },
-        NoiceLspProgressSpinner = { fg = colors.grey_fg },
-    }
-end
