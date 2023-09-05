cf5e8b1286de15541890a143ee050946031ef759
sownteedev <sownteenguyen@gmail.com>
2023-09-05 16:02:37 +0700

    add base46

---
+++ lua/themes/highlights.lua
@@ -0,0 +1,19 @@
+local M = {}
+local hl_files = vim.fn.stdpath "config" .. "/lua/themes/integrations"
+
+local highlights = {}
+
+function M.highlight_all(colors, opts)
+    if opts.transparent_background == true then
+        colors.background = "none"
+    end
+    for _, file in ipairs(vim.fn.readdir(hl_files)) do
+        local integration = require("themes.integrations." .. vim.fn.fnamemodify(file, ":r"))
+        highlights = vim.tbl_deep_extend("force", highlights, integration(colors))
+    end
+    for group, properties in pairs(highlights) do
+        vim.api.nvim_set_hl(0, group, properties)
+    end
+end
+
+return M
---
+++ lua/themes/schemes/rosepine.lua
@@ -0,0 +1,58 @@
+local M = {}
+
+function M.get_colors()
+    return {
+        -- base 30
+        black = "#191724", --  nvim bg
+        darker_black = "#13111e",
+        white = "#e0def4",
+        black2 = "#1f1d2a",
+        one_bg = "#262431",
+        one_bg2 = "#2d2b38",
+        one_bg3 = "#353340",
+        grey = "#3f3d4a",
+        grey_fg = "#474552",
+        grey_fg2 = "#514f5c",
+        light_grey = "#5d5b68",
+        red = "#eb6f92",
+        baby_pink = "#f5799c",
+        pink = "#ff83a6",
+        line = "#2e2c39",
+        green = "#ABE9B3",
+        vibrant_green = "#b5f3bd",
+        nord_blue = "#86b9c2",
+        blue = "#8bbec7",
+        yellow = "#f6c177",
+        sun = "#fec97f",
+        purple = "#c4a7e7",
+        dark_purple = "#bb9ede",
+        teal = "#6aadc8",
+        orange = "#f6c177",
+        cyan = "#a3d6df",
+        statusline_bg = "#201e2b",
+        lightbg = "#2d2b38",
+        pmenu_bg = "#c4a7e7",
+        folder_bg = "#6aadc8",
+        none = "NONE",
+
+        -- base16
+        base00 = "#191724",
+        base01 = "#1f1d2e",
+        base02 = "#26233a",
+        base03 = "#6e6a86",
+        base04 = "#908caa",
+        base05 = "#e0def4",
+        base06 = "#e0def4",
+        base07 = "#524f67",
+        base08 = "#eb6f92",
+        base09 = "#f6c177",
+        base0A = "#ebbcba",
+        base0B = "#31748f",
+        base0C = "#9ccfd8",
+        base0D = "#c4a7e7",
+        base0E = "#f6c177",
+        base0F = "#524f67",
+    }
+end
+
+return M
